8e6eeff62d4caf04244a39160b6fef23c4b901f0
Romain Ctd <chartaud.romain@hotmail.com>
2017-11-06 16:59:30 +0100

    premiere version

---
+++ js/video.js
@@ -0,0 +1,21 @@
+    var vid = document.getElementById("movie");
+var pauseButton = document.getElementById("vidpause");
+function vidFade() {
+  vid.classList.add("stopfade");
+}
+vid.addEventListener('ended', function() {
+  // only functional if "loop" is removed
+  vid.pause();
+  // to capture IE10
+  vidFade();
+});
+pauseButton.addEventListener("click", function() {
+  vid.classList.toggle("stopfade");
+  if (vid.paused) {
+    vid.play();
+    pauseButton.innerHTML = "STOP";
+  } else {
+    vid.pause();
+    pauseButton.innerHTML = "PLAY";
+  }
+})
